61b8c3c2abdace6946c1fc89bbc69ca7d893192c
Tongwu Zhang <xtmgah@gmail.com>
2020-07-17 21:17:42 -0400

    Update R scirpt and demo datasets

---
+++ Rscript/mSigPortal_Explore_example.R
@@ -0,0 +1,114 @@
+set_wd()
+library(tidyverse)
+library(cowplot)
+library(hrbrthemes)
+library(ggsci)
+library(ggrepel)
+library(ggforce)
+
+# source msigportal function ----------------------------------------------
+source('Sigvisualfunc.R')
+
+# load reference signatures files -----------------------------------------
+load('signature_refsets.RData')
+
+###  Signature Explore ###
+# summary of number of signatures or signature sets -----------------------
+nsig_data <-  signature_refsets %>% 
+  group_by(Profile,Signature_set_name) %>% 
+  summarise(N=n_distinct(Signature_name)) %>% 
+  ungroup() %>% 
+  mutate(Profile=factor(Profile,levels = c("SBS96","SBS192","SBS1536","ID83","DBS78","RS32")))
+
+nsig_data <- nsig_data %>% left_join(
+  nsig_data %>% group_by(Profile) %>% summarise(Total=sum(N)) 
+) %>% 
+  mutate(freq=N/Total) %>% 
+  mutate(N2=if_else(freq>0.02,as.character(N),""))
+
+
+# put the follow piechart on website 
+signature_piechart(nsig_data,sigsetcolor)
+
+
+# add signature SVG plots for all the signatures and put on line
+# three parameters to include: “Profile Type”, “Reference Signature Set”, “Signature Name” 
+profiletype <- "SBS" 
+signaturesetname <- "COSMIC v3 Signatures (SBS)"
+signaturename <- "SBS5"
+## according to these three parameters to pull out the svg files
+
+
+
+# cosine similarity heatmap between two signatures sets -------------------
+# The parameters will be “Matrix Size”, “Reference Signature Set1” and “Reference Signature Set2”. 
+matrixsize <- "SBS96" # profile type
+signaturesetname1 <- "Other published signatures (SBS)"
+signaturesetname2 <- "COSMIC v3 Signatures (SBS)"
+
+# the availabe options for signaturesetname1 and signaturesetname2 will be:
+signature_refsets %>% filter(Profile==matrixsize)%>% pull(Signature_set_name) %>% unique()
+
+
+sigrefset1 <- signature_refsets %>%
+  filter(Profile==matrixsize,Signature_set_name==signaturesetname1) %>% 
+  select(Signature_name,MutationType,Contribution) %>% 
+  pivot_wider(names_from = Signature_name,values_from=Contribution)
+
+sigrefset2 <- signature_refsets %>%
+  filter(Profile==matrixsize,Signature_set_name==signaturesetname2) %>% 
+  select(Signature_name,MutationType,Contribution) %>% 
+  pivot_wider(names_from = Signature_name,values_from=Contribution)
+
+cos_sim_res=cos_sim_df(sigrefset1,sigrefset2) 
+
+# put this heatmap on the web
+plot_cosine_heatmap_df(cos_sim_res,cluster_rows = TRUE,plot_values = FALSE)
+# add a link bellow the heatmap
+cos_sim_res %>% write_delim('signature_cos_sim_res.txt',delim = '\t',col_names = T)
+
+
+
+
+
+## A comparison of two reference signatures
+# There will be five parameters: “Profile Type”,  “Reference Signature Set1”, “Signature Name1”, “Reference Signature Set2”, “Signature Name2”; 
+matrixsize <- "SBS96" # profile type
+signaturesetname1 <- "COSMIC v3 Signatures (SBS)"
+signaturesetname2 <- "COSMIC v3 Signatures (SBS)"
+signaturename1 <-  "SBS1"
+signaturename2 <- "SBS5"
+
+# similarly, the availabe options for signaturesetname1 and signaturesetname2 will be:
+signature_refsets %>% filter(Profile==matrixsize)%>% pull(Signature_set_name) %>% unique()
+# the available options for the signature names will be:
+signature_refsets %>% filter(Profile==matrixsize,Signature_set_name==signaturesetname1)%>% pull(Signature_name) %>% unique()
+signature_refsets %>% filter(Profile==matrixsize,Signature_set_name==signaturesetname2)%>% pull(Signature_name) %>% unique()
+
+
+profile1 <- signature_refsets %>%
+  filter(Profile==matrixsize,Signature_set_name==signaturesetname1) %>% 
+  select(Signature_name,MutationType,Contribution) %>% 
+  pivot_wider(names_from = Signature_name,values_from=Contribution) %>% 
+  select(MutationType,one_of(signaturename1))
+
+profile2 <- signature_refsets %>%
+  filter(Profile==matrixsize,Signature_set_name==signaturesetname2) %>% 
+  select(Signature_name,MutationType,Contribution) %>% 
+  pivot_wider(names_from = Signature_name,values_from=Contribution) %>% 
+  select(MutationType,one_of(signaturename2))
+
+# put this plot on the web:
+plot_compare_profiles_diff(profile1,profile2,condensed = FALSE)
+
+
+
+
+
+# load exposure data files 
+
+
+# exposure association with genome data. 
+
+
+
--- Rscript/mSigPortal_Visualize_example.R
+++ Rscript/mSigPortal_Visualize_example.R
@@ -1,3 +1,4 @@
+set_wd()
 library(tidyverse)
 library(cowplot)
 library(hrbrthemes)
@@ -13,60 +14,141 @@ source('Sigvisualfunc.R')
 load('signature_refsets.RData')
 
 
-# on the web, we need the dropdown list for signature Profile Type and reference set name --------
-profilename <- "SBS"
-profilename2 <- if_else(profilename == "SBS","SBS96",if_else(profilename=="DBS","DBS78",if_else(profilename=="ID","ID84",NA_character_)))
-signaturesetname <- "COSMIC v3 Signatures (SBS)"
-
-signature_refsets_input <- signature_refsets %>% filter(Profile==profilename2,Signature_set_name==signaturesetname)
-
-
-# load SBS-96/DBS-78/ID-83 catalog file generated from previous result according to the profilename -----------
-if(profilename == "SBS"){
-  data_input <- read_delim('example_results/output/SBS/22520169-fe27-4549-ad3e-e78d1812c45e.SBS96.all',delim = '\t')
-}
-if(profilename == "DBS"){
-  data_input <- read_delim('example_results/output/DBS/22520169-fe27-4549-ad3e-e78d1812c45e.DBS78.all',delim = '\t')
-}
-if(profilename == "ID"){
-  data_input <- read_delim('example_results/output/ID/22520169-fe27-4549-ad3e-e78d1812c45e.ID83.all',delim = '\t')
-}
-
-## remove the false data from the result. Don't know why the web generated two additonal columns: False and seen, i think you trying to find the bug and remove. 
+### Cosine Similarity tab ###
+# section 1: Cosine similarity within samples # 
+# two parameters need: Profile Type and Matrix Size # 
+profiletype <- "SBS"
+matrixsize <- "SBS-96"
+matrixsize <- str_remove(matrixsize,"-")
+# data input can be filed use profiletype and matrixsize from the summary file jian generated. 
+data_input <- read_delim(paste0('example_results/output/',profiletype,'/22520169-fe27-4549-ad3e-e78d1812c45e.',matrixsize,'.all'),delim = '\t')
+## remove the false data from the result. Don't know why the web generated two additional columns: False and seen, i think you trying to find the bug and remove. 
 data_input <- data_input %>% select(-False,-seen)
+# Heatmap of cosine similarity within samples  and put on the web---------------------------
+cos_sim_res1=cos_sim_df(data_input,data_input) 
+plot_cosine_heatmap_df(cos_sim_res1,cluster_rows = TRUE,plot_values = FALSE)
+# a link to download the cosine similarity bellow the plot 
+# you could rename the file name if you need
+cos_sim_res1 %>% write_delim('cos_sim_res1.txt',delim = '\t',col_names = T)
 
-# Heatmap of cosine similarity to reference set ---------------------------
 
+# section 2: Cosine similarity  to reference signatures # 
+# Two parameters need: Profile Type, Reference Signature Set
+# Profile Type only support SBS, DBS, ID
+profiletype <- "SBS"
+profilename <- if_else(profiletype == "SBS","SBS96",if_else(profiletype=="DBS","DBS78",if_else(profiletype=="ID","ID83",NA_character_)))
+#all the option of Reference Signature Set
+signature_refsets %>% filter(Profile==profilename) %>% pull(Signature_set_name) %>% unique() ## put on the dropdown list 
+# if selected "COSMIC v3 Signatures (SBS)"
+signaturesetname <- "COSMIC v3 Signatures (SBS)"
+signature_refsets_input <- signature_refsets %>% filter(Profile==profilename,Signature_set_name==signaturesetname)
 sigref <- signature_refsets_input %>% 
   select(Signature_name,MutationType,Contribution) %>% 
   pivot_wider(names_from = Signature_name,values_from=Contribution)
 
-data_input
-cos_sim_res=cos_sim_df(data_input,sigref) 
+matrixsize <- profilename
+# load SBS-96/DBS-78/ID-83 catalog files generated from previous result according to the profilename -----------
+data_input <- read_delim(paste0('example_results/output/',profiletype,'/22520169-fe27-4549-ad3e-e78d1812c45e.',matrixsize,'.all'),delim = '\t')
+## remove the false data from the result. Don't know why the web generated two additional columns: False and seen, i think you trying to find the bug and remove. 
+data_input <- data_input %>% select(-False,-seen)
 
-# put this heatmap on the web
-plot_cosine_heatmap_df(cos_sim_res,cluster_rows = TRUE,plot_values = FALSE)
+# Heatmap of cosine similarity to reference set signature and put on the web---------------------------
+cos_sim_res2=cos_sim_df(data_input,sigref) 
+plot_cosine_heatmap_df(cos_sim_res2,cluster_rows = TRUE,plot_values = FALSE)
+# a link to download the cosine similarity bellow the plot 
+# you could rename the file name if you need
+cos_sim_res2 %>% write_delim('cos_sim_res2.txt',delim = '\t',col_names = T)
 
 
-# on the web, dropdown list for the input sample and (specific signature or a formula), put the barplot on the web
-sample_input <-  "SC420396"
 
-# option1: specific signature
-siganture_name_input <- "SBS5"
-profile1 <- data_input %>% select(MutationType,one_of(sample_input))
-profile2 <- sigref %>% select(MutationType,one_of(siganture_name_input))
+
+
+
+
+
+
+### Profile Comparison tab ### 
+# section 1: Cosine similarity within samples # 
+# three parameters need: Profile Type, Sample Name1 and Sample Name2 # 
+profiletype <- "SBS"
+sample_name1 <-  "SB749362"
+sample_name2 <-  "SC420396"
+
+matrixsize <- if_else(profiletype == "SBS","SBS96",if_else(profiletype=="DBS","DBS78",if_else(profiletype=="ID","ID83",NA_character_)))
+data_input <- read_delim(paste0('example_results/output/',profiletype,'/22520169-fe27-4549-ad3e-e78d1812c45e.',matrixsize,'.all'),delim = '\t')
+## remove the false data from the result. Don't know why the web generated two additional columns: False and seen, i think you trying to find the bug and remove. 
+data_input <- data_input %>% select(-False,-seen)
+
+profile1 <- data_input %>% select(MutationType,contains(sample_name1))
+profile2 <- data_input %>% select(MutationType,contains(sample_name2))
+# put the plot on the web
 plot_compare_profiles_diff(profile1,profile2,condensed = FALSE)
 
-# option2: formula input
-formula_input <- "0.8*SBS5;0.1*SBS1;0.1*SBS3"
-profile1 <- data_input %>% select(MutationType,one_of(sample_input))
-profile2 <- signature_sum_operation(sigdatabase = signature_refsets_input,sigsetname = signaturesetname,formulax = formula_input,outsigname = "Reconstructed")
-plot_compare_profiles_diff(profile1,profile2,condensed = FALSE,profile_names = c("Original","Reconstructed"))
+
+# section 2: Comparison to reference signatures # 
+# four parameters need: “Profile Type”, “Sample Name”, “Reference Signature Set” and “Compare Single Signature or Combined Signatures” # 
+sample_name <-  "SC420396"
+profiletype <- "SBS"
+profilename <- if_else(profiletype == "SBS","SBS96",if_else(profiletype=="DBS","DBS78",if_else(profiletype=="ID","ID83",NA_character_)))
+matrixsize <- profilename
+
+data_input <- read_delim(paste0('example_results/output/',profiletype,'/22520169-fe27-4549-ad3e-e78d1812c45e.',matrixsize,'.all'),delim = '\t')
+profile1 <- data_input %>% select(MutationType,one_of(sample_name))
+
+#all the option of Reference Signature Set
+signature_refsets %>% filter(Profile==profilename) %>% pull(Signature_set_name) %>% unique() ## put on the dropdown list 
+# if selected "COSMIC v3 Signatures (SBS)"
+signaturesetname <- "COSMIC v3 Signatures (SBS)"
+signature_refsets_input <- signature_refsets %>% filter(Profile==profilename,Signature_set_name==signaturesetname)
+sigref <- signature_refsets_input %>% 
+  select(Signature_name,MutationType,Contribution) %>% 
+  pivot_wider(names_from = Signature_name,values_from=Contribution)
+
+user_input <- "SBS5" #user_input <- "0.8*SBS5;0.1*SBS1;0.1*SBS3"
+
+if(str_detect(user_input,";")){
+  profile2 <- signature_sum_operation(sigdatabase = signature_refsets_input,sigsetname = signaturesetname,formulax = user_input,outsigname = "Reconstructed")
+  profile_names = c("Original","Reconstructed")
+}else {
+  profile2 <- sigref %>% select(MutationType,one_of(user_input))
+  profile_names = c(colnames(profile1)[2],colnames(profile2)[2])
+}
+
+plot_compare_profiles_diff(profile1,profile2,condensed = FALSE,profile_names = profile_names)
+
+
+
+
+
+
+
+
+
+
+### “Principal Component Analysis” ###
+# Two parameters need: Profile Type, Reference Signature Set
+# Profile Type only support SBS, DBS, ID
+profiletype <- "SBS"
+profilename <- if_else(profiletype == "SBS","SBS96",if_else(profiletype=="DBS","DBS78",if_else(profiletype=="ID","ID83",NA_character_)))
+matrixsize <- profilename
+#all the option of Reference Signature Set
+signature_refsets %>% filter(Profile==profilename) %>% pull(Signature_set_name) %>% unique() ## put on the dropdown list 
+# if selected "COSMIC v3 Signatures (SBS)"
+signaturesetname <- "COSMIC v3 Signatures (SBS)"
+signature_refsets_input <- signature_refsets %>% filter(Profile==profilename,Signature_set_name==signaturesetname)
+sigref <- signature_refsets_input %>% 
+  select(Signature_name,MutationType,Contribution) %>% 
+  pivot_wider(names_from = Signature_name,values_from=Contribution)
+
+
+# load SBS-96/DBS-78/ID-83 catalog files generated from previous result according to the profilename -----------
+data_input <- read_delim(paste0('example_results/output/',profiletype,'/22520169-fe27-4549-ad3e-e78d1812c45e.',matrixsize,'.all'),delim = '\t')
+## remove the false data from the result. Don't know why the web generated two additional columns: False and seen, i think you trying to find the bug and remove. 
+data_input <- data_input %>% select(-False,-seen)
 
 
 # PCA plot ----------------------------------------------------------------
 
-data_input
 mdata_input <- as.matrix(data_input[,-1])
 rownames(mdata_input) <- data_input$MutationType
 
@@ -79,6 +161,9 @@ if((dim(data_input)[2]-1) > 20){
   fviz_pca_ind(res.pca,axes = c(1,2),col.ind="contrib",gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),repel = TRUE)
 }
 
+## a link to download pca data1
+res.pca$x %>% as.data.frame() %>% rownames_to_column(var = 'Sample') %>% write_delim('PCA1.txt',delim = '\t',col_names = T)
+
 
 fviz_pca_var(res.pca,axes = c(1, 2),
              col.var = "contrib", # Color by contributions to the PC
@@ -86,16 +171,26 @@ fviz_pca_var(res.pca,axes = c(1, 2),
              repel = FALSE     # Avoid text overlapping
 )
 
-## output PCAs
-res.pca$x 
-res.pca$rotation
+# a link to download pca data2
+res.pca$rotation %>% as.data.frame() %>% rownames_to_column(var = 'Sample') %>% write_delim('PCA2.txt',delim = '\t',col_names = T)
+
+
 
 ## heatmap between PCs and signatures
 sigpca <-  res.pca$rotation %>% as.data.frame() %>% rownames_to_column(var = "MutationType")
-cos_sim_res=cos_sim_df(sigpca,sigref) 
-
+cos_sim_res3=cos_sim_df(sigpca,sigref) 
 # put this heatmap on the web
-plot_cosine_heatmap_df(cos_sim_res,cluster_rows = TRUE,plot_values = FALSE)
+plot_cosine_heatmap_df(cos_sim_res3,cluster_rows = TRUE,plot_values = FALSE)
+# a link to download the cosine similarity bellow the plot 
+# you could rename the file name if you need
+cos_sim_res2 %>% write_delim('cos_sim_res3.txt',delim = '\t',col_names = T)
+
+
+
+
+
+
+
 
 
 
